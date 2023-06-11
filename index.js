201017d968f995330d20af0710ac62267c4b774a
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-12 01:20:37 +0600

    jtw post added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -29,6 +30,14 @@ async function run() {
         await client.connect();
         /* Works here */
         const usersCollection = client.db("languageDb").collection("users");
+
+        // JWT Token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res.send({ token })
+        })
         // For saving registered user
         app.post('/users', async (req, res) => {
             const user = req.body;
