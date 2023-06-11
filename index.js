dd23e41551e510b9e2d1b355aa14b00dd1aaa7d8
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-11 21:40:06 +0600

    mongo with env added

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Mongo start here
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.clbkfrr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+/* Works here */
+
+/* Working zone end */
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// Mongo End here
 
 app.get('/', (req, res) => {
     res.send('Language Oasis Server is running')
